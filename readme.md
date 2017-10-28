ae06728b40f1c8336180167587dcc993d46b4fac
instrumaniak <instrumaniak@gmail.com>
2017-10-28 11:48:25 +0600

    more clean up.

--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
 # Event Calendar
 
-This a demo fullstack javascipt web app.
+This a demo fullstack javascipt web app. This is based on angular-ui-calendar project's demo. The front end (angularJS) loads the calendar events (json) via $http service.
 
 view live demo from here: https://calendar-webapp.herokuapp.com/
 
+## How to run locally
+
+Tested with: Node: 6.X, NPM: 5.X
+
+Clone this repository and enter into it, then from command line run:
+
+```bash
+npm install
+bower install
+npm start
+```
--- ui/calendarDemo.js
+++ ui/calendarDemo.js
@@ -1,27 +1,23 @@
 /**
- * calendarDemoApp - 0.9.0
+ * calendarDemoApp - from angular-ui-caldender demo
+ * clean up & adaption by Raziur Rahman, 2017
  */
 var calendarDemoApp = angular.module('calendarDemoApp', ['ui.calendar', 'ui.bootstrap']);
 
 calendarDemoApp.controller('CalendarCtrl',
-   function($scope, $compile, $timeout, $http, uiCalendarConfig) {
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-   // $scope.eventSources = []    ;
+   function($scope, $http, uiCalendarConfig) {
+   
     $scope.events = [];
 
     $http.get('/events.json').then(function(response){
       var data = response.data;
-      //$scope.eventSource = [];
+      
       angular.forEach(data, function(data){
         $scope.events.push({
           id: data.id,
           title: data.title,
-          start: new Date(data.start),
-          end: new Date(data.end),
+          start: data.start,
+          end: data.end,
           url: data.url,
           stick: true
         })
@@ -31,27 +27,9 @@ calendarDemoApp.controller('CalendarCtrl',
     });
 
 
-    /* event source that calls a function on every view switch */
-    $scope.eventsF = function (start, end, timezone, callback) {
-      var s = new Date(start).getTime() / 1000;
-      var e = new Date(end).getTime() / 1000;
-      var m = new Date(start).getMonth();
-      var events = [{title: 'Feed Me ' + m,start: s + (50000),end: s + (100000),allDay: false, className: ['customFeed']}];
-      callback(events);
-    };
-
-    $scope.calEventsExt = {
-       color: '#f00',
-       textColor: 'yellow',
-       events: [
-          {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-          {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
-        ]
-    };
-    /* alert on eventClick */
     $scope.alertOnEventClick = function( date, jsEvent, view){
         $scope.alertMessage = (date.title + ' was clicked ');
+        
     };
     /* alert on Drop */
      $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
@@ -63,31 +41,23 @@ calendarDemoApp.controller('CalendarCtrl',
     };
    
     /* add custom event*/
-    $scope.addEvent = function() {
-      $scope.events.push({
-        title: 'Open Sesame',
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        className: ['openSesame']
-      });
+    $scope.addEvent = function(start, end) {
+      var title = prompt("Enter title");
+      if(title){
+          $scope.events.push({
+            title: title,
+            start: start,
+            end: end,
+            stick: true
+          });
+        }
+      uiCalendarConfig.calendars.myCalendar.fullCalendar('unselect');
     };
     /* remove event */
     $scope.remove = function(index) {
       $scope.events.splice(index,1);
     };
-    /* Change View */
-    $scope.changeView = function(view,calendar) {
-      uiCalendarConfig.calendars[calendar].fullCalendar('changeView',view);
-    };
-    /* Change View */
-    $scope.renderCalendar = function(calendar) {
-      $timeout(function() {
-        if(uiCalendarConfig.calendars[calendar]){
-          uiCalendarConfig.calendars[calendar].fullCalendar('render');
-        }
-      });
-    };
-     
+   
     /* config object */
     $scope.uiConfig = {
       calendar:{
@@ -97,6 +67,8 @@ calendarDemoApp.controller('CalendarCtrl',
         selectHelper: true,
         eventDurationEditable: true,
         eventResourceEditable: true,
+        eventLimit: true,
+        navLinks: true,
         header:{
           center: 'title',
           left: 'month,agendaWeek,agendaDay',
@@ -104,7 +76,8 @@ calendarDemoApp.controller('CalendarCtrl',
         },
         eventClick: $scope.alertOnEventClick,
         eventDrop: $scope.alertOnDrop,
-        eventResize: $scope.alertOnResize
+        eventResize: $scope.alertOnResize,
+        select: $scope.addEvent
         
         
       }
@@ -112,7 +85,7 @@ calendarDemoApp.controller('CalendarCtrl',
 
     
     /* event sources array*/
-    $scope.eventSources = [$scope.events, $scope.eventsF];
+    $scope.eventSources = [$scope.events];
     
 });
 /* EOF */
