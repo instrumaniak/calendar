30afefcc01d6719ba4a612f9608ec335e8261660
Raziur Rahman <instrumaniak@gmail.com>
2019-04-01 19:04:53 +0600

    ui: added static calendar toolbar on navbar.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,7 +10,22 @@ import {
   NavLink
 } from 'reactstrap'
 
-import { Calendar } from 'react-feather'
+import { Calendar, Square, ChevronLeft, ChevronRight, Settings, User, Search } from 'react-feather'
+
+const CalendarToolBar = () => {
+  return (
+    <div className='c-calendar-toolbar'>
+
+      <span className='title'>April 2019 </span>
+
+      <span className='icon-set'>
+        <ChevronLeft />
+        <ChevronRight />
+        <Square className='ml-2' size={20} />
+      </span>
+    </div>
+  )
+}
 
 const NavBar = () => {
   const [ isOpen, setIsOpen ] = useState(false) // state for toggling navbar
@@ -23,9 +38,16 @@ const NavBar = () => {
       <NavbarBrand tag={Link} to='/' onClick={closeNav}><Calendar/> Calendar</NavbarBrand>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
+        <CalendarToolBar />
         <Nav className='ml-auto' navbar>
           <NavItem>
-            <NavLink tag={RRNavLink} to='/about' onClick={closeNav}>About</NavLink>
+            <NavLink tag={RRNavLink} to='/about' onClick={closeNav}><Search/></NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to='/about' onClick={closeNav}><Settings/></NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to='/about' onClick={closeNav}><User/></NavLink>
           </NavItem>
         </Nav>
       </Collapse>
