f5496cb2d3c59a4de54c283827abd78ff9003923
Raziur Rahman <instrumaniak@gmail.com>
2019-07-15 14:14:55 +0600

    frontend: connected home page calendar to api data.

--- client/src/components/PageHome.js
+++ client/src/components/PageHome.js
@@ -1,27 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment'
 
 const localizer = momentLocalizer(moment)
 
 const PageHome = () => {
+  const [ events, setEvents ] = useState([])
+
+  useEffect(()=>{
+    fetch('/api/events')
+      .then(res => res.json())
+      .then(data => setEvents(data))
+  }, [])
+
   return (
     <div className='c-bigcalendar-container'>
       <Calendar
         localizer={localizer}
         toolbar={false}
-        events={[
-          {
-            title: 'Test Event',
-            start: new Date(),
-            end: new Date()
-          },
-          {
-            title: 'Test Event 2',
-            start: new Date(),
-            end: new Date()
-          }
-        ]}
+        events={events}
       />
     </div>
   )
