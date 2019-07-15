9d91d158ee3dd80b342c799dded414d0d6837633
Raziur Rahman <instrumaniak@gmail.com>
2019-07-15 16:00:41 +0600

    frontend: added routing from home to event edit & connected to api data

--- client/src/components/PageEventEdit.js
+++ client/src/components/PageEventEdit.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const PageEventEdit = () => {
+const PageEventEdit = ({ match }) => {
+  const [ eventData, setEventData ] = useState({
+    _id: '',
+    title: '',
+    start: '',
+    end: '',
+    color: '',
+    info: ''
+  })
+
+  useEffect(()=>{
+    fetch(`/api/events/${match.params.id}`)
+      .then(res => res.json())
+      .then(data => setEventData({
+        _id: data._id || '',
+        title: data.title || '',
+        start: data.start || '',
+        end: data.end || '',
+        color: data.color || '',
+        info: data.info || ''
+      }))
+      .catch(err => console.log(err))
+  },[match.params.id])
+
   return (
     <div className='c-page-container'>
       <form>
@@ -9,39 +32,39 @@ const PageEventEdit = () => {
           <label className="control-label " htmlFor="title">
             Title
           </label>
-          <input className="form-control" id="title" name="title" type="text" />
+          <input className="form-control" name="title" type="text" value={eventData.title} />
         </div>
         <div className="form-group ">
           <label className="control-label " htmlFor="start">
             Start Date
           </label>
-          <input className="form-control" id="start" name="start" type="text" />
+          <input className="form-control" name="start" type="text" value={eventData.start}/>
         </div>
         <div className="form-group ">
           <label className="control-label " htmlFor="end">
             End Date
           </label>
-          <input className="form-control" id="end" name="end" type="text" />
+          <input className="form-control" name="end" type="text" value={eventData.end}/>
           <small className="hint">* End date is exclusive in the date range.</small>
         </div>
         <div className="form-group ">
           <label className="control-label " htmlFor="info">
             Details
           </label>
-          <textarea className="form-control" cols={40} id="info" name="info" rows={5} defaultValue={""} />
+          <textarea className="form-control" cols={40} name="info" rows={5} value={eventData.info}/>
         </div>
         <div className="form-group ">
           <label className="control-label " htmlFor="color">
             Color
           </label>
-          <input className="form-control" id="color" name="color" type="text" />
+          <input className="form-control" name="color" type="text" value={eventData.color}/>
           <small className="hint">* Valid HTML color. Like: red, #eee, #FFEE00.</small>
         </div>
         <div className="form-group">
           <div>
             <button className="btn btn-secondary">Cancel</button>{' '}
             <button className="btn btn-danger">Delete</button>{' '}
-            <button className="btn btn-primary">
+            <button className="btn btn-primary" disabled>
               Save
             </button>
           </div>
--- client/src/components/PageHome.js
+++ client/src/components/PageHome.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Calendar, momentLocalizer} from 'react-big-calendar'
 import moment from 'moment'
 
@@ -6,6 +7,7 @@ const localizer = momentLocalizer(moment)
 
 const PageHome = () => {
   const [ events, setEvents ] = useState([])
+  const [ selectedEventID, setSelectedEventID ] = useState('')
 
   useEffect(()=>{
     fetch('/api/events')
@@ -13,12 +15,23 @@ const PageHome = () => {
       .then(data => setEvents(data))
   }, [])
 
+  const handleEventSelect = (event, e) => {
+    if(event._id) {
+      setSelectedEventID(event._id)
+    }
+  }
+
+  if(selectedEventID.length > 0) {
+    return <Redirect to={`/event/${selectedEventID}`} />
+  }
+
   return (
     <div className='c-bigcalendar-container'>
       <Calendar
         localizer={localizer}
         toolbar={false}
         events={events}
+        onSelectEvent={handleEventSelect}
       />
     </div>
   )
