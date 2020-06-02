6100fb1fd032039f6240a1954c642dd3f1925261
Raziur Rahman <instrumaniak@gmail.com>
2020-06-02 19:14:37 +0600

    BE: added swagger documentation setup + doc  #14

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const eventsRouter = require('./events')
 const usersRouter = require('./users')
+const docsRouter = require('./docs')
 
-router.use('/events', eventsRouter)
-router.use('/users', usersRouter)
+router
+  .use('/events', eventsRouter)
+  .use('/users', usersRouter)
+  .use('/docs', docsRouter)
 
 module.exports = router
