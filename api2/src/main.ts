b93183280c2cdd7334e57cb21f42525f8bbe7c86
Raziur Rahman <instrumaniak@gmail.com>
2020-07-27 18:19:28 +0600

    BE: setup swagger for nestjs #14

--- api2/src/main.ts
+++ api2/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './modules/app/app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
+
+  // setup swagger
+  const options = new DocumentBuilder()
+    .setTitle('Calendar')
+    .setDescription('Calendar API documentation')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api/docs', app, document)
+
   await app.listen(process.env.PORT || 5000)
 
   console.log('\nListening on: ', await app.getUrl())
