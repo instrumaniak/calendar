65936ff4f415ff893a6182b0e049240043f3419f
Raziur Rahman <instrumaniak@gmail.com>
2020-08-01 03:13:51 +0600

    BE: setup nestjs for serving react frontend build #25

--- api/src/modules/app/app.module.ts
+++ api/src/modules/app/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { EventsModule } from '../events/events.module'
+import { join } from 'path'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), EventsModule]
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../../../client/build'),
+      exclude: ['/api*']
+    }),
+    TypeOrmModule.forRoot(),
+    EventsModule
+  ]
 })
 export class AppModule {}
