bdecf0675fa9fb371a8fc9441b7874d1b0c63c54
Raziur Rahman <instrumaniak@gmail.com>
2020-08-01 04:40:51 +0600

    BE:  postgres  instead of mysql for heroku #25

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,11 +1,11 @@
 const commonOptions = {
-  type: 'mysql',
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  logging: true
+  logging: process.env.NODE_ENV !== 'production'
 }
 
 module.exports = [
